a9ed4f602c6e0bb2ca63021bb669e78b673c5373
AryaanMerchant <71880976+AryaanMerchant@users.noreply.github.com>
2020-11-30 17:41:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var game,database;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    console.log(database)
+
+    
+    game = new Game();
+    game.getState();
+    game.start();
+
+}
+
+function draw(){
+    background("white");
+
+   
+}
+
+
